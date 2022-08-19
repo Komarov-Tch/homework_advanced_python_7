8b89f4003292f5229d7f0afaf67279bdf3847ac2
Egor_Komarov <75240295+Komarov-Tch@users.noreply.github.com>
2022-08-19 20:48:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 class Stack:
-    def __init__(self, st):
+    def __init__(self, st=[]):
         self.st = st
 
     def isEmpty(self):
         if self.st:
-            return True
-        else:
             return False
+        else:
+            return True
 
     def push(self, new_elem):
-        if isinstance(self.st, str):
-            self.st += new_elem
-        elif isinstance(self.st, list):
-            self.st.append(new_elem)
+        self.st.append(new_elem)
 
     def pop(self):
-        last_elem = self.st[-1]
-        self.st = self.st[:-1]
-        return last_elem
+        return self.st.pop()
 
     def peek(self):
-        last_elem = self.st[-1]
-        return last_elem
+        return self.st[-1]
 
     def size(self):
         return len(self.st)
@@ -30,32 +24,25 @@ class Stack:
         return f'{self.st}'
 
 
-def parity(s: Stack) -> bool:
-    if s.size() % 2 == 0:
-        return True
-    return False
-
-
-def balance(s: Stack) -> bool:
-    skobki = {}
-    for _ in range(s.size()):
-        skobki[s.pop()] = skobki.get(s.peek(), 0) + 1
-    if all([skobki.get('(', 0) == skobki.get(')', 0),
-            skobki.get('[', 0) == skobki.get(']', 0),
-            skobki.get('{', 0) == skobki.get('}', 0)]):
-        return True
-    return False
+def balance(hooks: str) -> Stack:
+    dict_hooks = {'(': ')', '{': '}', '[': ']'}
+    s = Stack()
+    for h in hooks:
+        if h in dict_hooks.keys():
+            s.push(h)
+        elif not s.isEmpty() and dict_hooks.get(s.peek(), '') == h:
+            s.pop()
+        else:
+            s.push(h)
+            break
+    return s
 
 
 def main():
     enter = input('Введите скобочную последовательность: ')
     print()
-    str_hooks = Stack(enter)
-    if parity(str_hooks):
-        result = balance(str_hooks)
-    else:
-        result = False
-    if result:
+    result = balance(enter)
+    if result.isEmpty():
         print('Сбалансированно')
     else:
         print('Несбалансированно')
@@ -63,3 +50,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
