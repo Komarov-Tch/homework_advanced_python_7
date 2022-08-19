7c99eb8698378f041fd14cd21d61277da52c7912
Komarov-Tch <komarovegr@gmail.com>
2022-08-19 15:55:13 +0700

    Доп задание.

--- refact.py
+++ refact.py
@@ -1,61 +1,68 @@
 import email
 import smtplib
 import imaplib
-from email.MIMEText import MIMEText
-from email.MIMEMultipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+class Mail:
+    def __init__(self, login: str, password: str,
+                 subject: str, recipients: list, message: str, header):
+        self.login = login
+        self.password = password
+        self.subject = subject
+        self.recipients = recipients
+        self.message = message
+        self.header = header
+
+    def sendmessage(self):
+        msg = MIMEMultipart()
+        msg['From'] = self.login
+        msg['To'] = ', '.join(self.recipients)
+        msg['Subject'] = self.subject
+        msg.attach(MIMEText(self.message))
+        self.__security(msg)
+
+    def __security(self, msg):
+        ms = smtplib.SMTP(GMAIL_SMTP, 587)
+        ms.ehlo()
+        ms.starttls()
+        ms.ehlo()
+        ms.login(self.login, self.password)
+        ms.sendmail(self.login, ms, msg.as_string())
+        ms.quit()
+
+    def recievemessage(self):
+        mail = imaplib.IMAP4_SSL(GMAIL_IMAP)
+        mail.login(self.login, self.password)
+        mail.list()
+        mail.select('inbox')
+        criterion = '(HEADER Subject "%s")' % self.header if self.header else 'ALL'
+        result, data = mail.uid('search', None, criterion)
+        assert data[0], 'There are no letters with current header'
+        latest_email_uid = data[0].split()[-1]
+        result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+        raw_email = data[0][1]
+        email_message = email.message_from_string(raw_email)
+        mail.logout()
+
 
 GMAIL_SMTP = "smtp.gmail.com"
 GMAIL_IMAP = "imap.gmail.com"
 
-l = 'login@gmail.com'
-passwORD = 'qwerty'
-subject = 'Subject'
-recipients = ['vasya@email.com', 'petya@email.com']
-message = 'Message'
-header = None
-
-# send message
-msg = MIMEMultipart()
-msg['From'] = l
-msg['To'] = ', '.join(recipients)
-msg['Subject'] = subject
-msg.attach(MIMEText(message))
-
-ms = smtplib.SMTP(GMAIL_SMTP, 587)
-# identify ourselves to smtp gmail client
-ms.ehlo()
-# secure our email with tls encryption
-ms.starttls()
-# re-identify ourselves as an encrypted connection
-ms.ehlo()
-
-ms.login(l, passwORD)
-ms.sendmail(l,
-            ms, msg.as_string())
-
-ms.quit()
-# send end
-
-
-# recieve
-mail = imaplib.IMAP4_SSL(GMAIL_IMAP)
-mail.login(l, passwORD)
-mail.list()
-mail.select("inbox")
-criterion = '(HEADER Subject "%s")' % header if header else 'ALL'
-result, data = mail.uid('search', None, criterion)
-assert data[0], 'There are no letters with current header'
-latest_email_uid = data[0].split()[-1]
-result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-raw_email = data[0][1]
-email_message = email.message_from_string(raw_email)
-mail.logout()
-
-
-# end recieve
 
 def main():
-    pass
+    login = 'login@gmail.com'
+    password = 'qwerty'
+    subject = 'Subject'
+    recipients = ['vasya@email.com', 'petya@email.com']
+    message = 'Message'
+    header = None
+    new_message = Mail(login=login, password=password,
+                       subject=subject, recipients=recipients,
+                       message=message, header=header)
+    new_message.sendmessage()
+    new_message.recievemessage()
 
 
 if __name__ == '__main__':
